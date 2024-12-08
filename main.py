97ba5289db9c9a9fd01ffb6257d99b3ecdc5eda1
Arya <83648588+awesome110@users.noreply.github.com>
2024-12-08 21:29:25 +0000

    Added sidebar test 2

--- main.py
+++ main.py
@@ -37,17 +37,13 @@ def login_page():
             st.error("Invalid username or password.")
 
 def app_page():
-    add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-    )
-
-    # Using "with" notation
     with st.sidebar:
-        add_radio = st.radio(
-            "Choose a shipping method",
-            ("Standard (5-15 days)", "Express (2-5 days)")
-        )
+        with st.echo():
+            st.write("This code will be printed to the sidebar.")
+
+        with st.spinner("Loading..."):
+            time.sleep(5)
+        st.success("Done!")
 
     st.title('CerviHope')
     #setting the main picture
